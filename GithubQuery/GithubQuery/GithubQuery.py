fb6a1d20aabbd10bb44bd2061d3a74634107277c
Bence Fazekas <fazon.bence@gmail.com>
2020-02-04 21:04:08 +0100

    Get all query pages

--- GithubQuery/GithubQuery/GithubQuery.py
+++ GithubQuery/GithubQuery/GithubQuery.py
@@ -1,6 +1,8 @@
 import requests
 from requests.auth import HTTPDigestAuth
 import json
+import itertools
+from time import sleep
 
 MyOauth2Token = 'd9799af140fe1be693a8ab74584e8f6e009a463f'
 url = "https://api.github.com/search/repositories?q"
@@ -31,22 +33,31 @@ keys = [
 ]
 
 #qeuery parameters
-queryParams = 'maven2+in:readme'
-parameters = {
-    "page": 2,
-    "q": queryParams
-    }
+queryParams = 'https://repo1.maven.org/+in:readme'
 
 
-with requests.Session() as s:
+resultlist = []
 
-    s.headers.update(headers)
-    resp = s.get(url, params=parameters)
-    #jprint(resp.json())
+#loop until Github max query limit
+for i in range(2):
+#for i in itertools.count():
+    with requests.Session() as s:
+        parameters = {
+            "page": i,
+            "q": queryParams
+            }
+        s.headers.update(headers)
+        resp = s.get(url, params=parameters)
+        #if the sessions is OK
+        if resp.status_code == 200:
+            resultlist.append([{key:item[key] for key in keys} for item in resp.json()["items"]])
+            sleep(0.05)
+        #break if Github deny more result
+        else:
+            break
 
-response = resp.json()
+#merge the lists
+resultlist = list(itertools.chain.from_iterable(resultlist))
 
-
-#filter results
-FilteredResults = [{key:item[key] for key in keys} for item in response["items"]]
-jprint(FilteredResults)
+jprint(resultlist)
+print(len(resultlist))
