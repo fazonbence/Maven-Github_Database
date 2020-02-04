2198d6a6af33c1c9675c599e6952d9383fda87f1
Bence Fazekas <fazon.bence@gmail.com>
2020-02-04 16:51:43 +0100

    Getting a single page from the Query Added html header http header authorization

--- GithubQuery/GithubQuery/GithubQuery.py
+++ GithubQuery/GithubQuery/GithubQuery.py
@@ -1,2 +1,24 @@
+import requests
+from requests.auth import HTTPDigestAuth
+import json
 
-print("initial commit")
+MyOauth2Token = 'd9799af140fe1be693a8ab74584e8f6e009a463f'
+url = "https://api.github.com/search/repositories?q"
+
+
+
+headers = { 'Authorization' : 'token ' + MyOauth2Token }
+
+queryParams = 'maven2+in:readme'
+parameters = {
+    "page": 2,
+    "q": queryParams
+    }
+
+
+with requests.Session() as s:
+
+    s.headers.update(headers)
+    resp = s.get(url, params=parameters)
+    jprint(resp.json())
+
