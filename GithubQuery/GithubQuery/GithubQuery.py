41503d81adcfce3ec76f9d00a29b22f9b36d8dfa
Bence Fazekas <fazon.bence@gmail.com>
2020-02-05 10:21:50 +0100

    GetCommitList function: collect commit informations, from the selected projects

--- GithubQuery/GithubQuery/GithubQuery.py
+++ GithubQuery/GithubQuery/GithubQuery.py
@@ -11,11 +11,10 @@ import json
 import itertools
 from time import sleep
 
-
 MyOauth2Token = 'd9799af140fe1be693a8ab74584e8f6e009a463f'
 headers = { 'Authorization' : 'token ' + MyOauth2Token }
 
-def getList(dict): 
+def getDictKeys(dict): 
     """returns all keys from a dictionary as a list"""
     list = [] 
     for key in dict.keys(): 
@@ -30,15 +29,16 @@ def jprint(obj):
 
 def GetRepoList():
     #collects the github repository informations via Github API
-
     
-    repo_url = "https://api.github.com/search/repositories?q"
+    url_repo = "https://api.github.com/search/repositories?q"
     #Which properties are needed
-    keys = [
+    keys_repo = [
         "html_url",
+        "url",
          "stargazers_count",
          "watchers_count",
-         "forks_count"
+         "forks_count",
+         "commits_url"
     ]
     #qeuery parameters
     queryParams = 'https://repo1.maven.org/+in:readme'
@@ -54,10 +54,10 @@ def GetRepoList():
                 "q": queryParams
                 }
             s.headers.update(headers)
-            resp = s.get(repo_url, params=parameters)
+            resp = s.get(url_repo, params=parameters)
             #if the sessions is OK
             if resp.status_code == 200:
-                resultlist.append([{key:item[key] for key in keys} for item in resp.json()["items"]])
+                resultlist.append([{key:item[key] for key in keys_repo} for item in resp.json()["items"]])
                 sleep(0.05)
             #break if Github deny more result
             else:
@@ -67,4 +67,42 @@ def GetRepoList():
     resultlist = list(itertools.chain.from_iterable(resultlist))
     return resultlist
 
-jprint(GetRepoList())
+def GetCommitList(RepoList):
+    #Which properties are needed
+    keys_repo = [
+        "html_url",
+        "url",
+         "parents",
+         "sha",
+         "commit"
+    ]
+    #qeuery parameters
+    queryParams = 'bug+in:message'
+
+    resultlist = []
+    #for i in range(2):
+    for i in itertools.count():
+        with requests.Session() as s:
+            parameters = {
+                "page": i
+                ,"q": queryParams
+                }
+            s.headers.update(headers)
+            print(RepoList[0]["commits_url"][:-6])
+            resp = s.get(RepoList[0]["commits_url"][:-6], params=parameters)
+
+            #jprint(resp.json())
+            #if the sessions is OK
+            if resp.status_code == 200:
+                print(getDictKeys(resp.json()[0]))
+                resultlist.append([{key:item[key] for key in keys_repo} for item in resp.json()])
+                sleep(0.05)
+            #break if Github deny more result
+            else:
+                break
+
+    
+    return resultlist
+
+jprint(GetCommitList(GetRepoList()))
+#GetCommitList(GetRepoList())
